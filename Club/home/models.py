a4fa61ea1bb4d2168a981e46b69061eb060ebe98
Wannes Van Praet <Wannesvp@users.noreply.github.com>
2018-06-13 22:24:05 +0200

    JS Fullcalendar first working implementation

--- Club/home/models.py
+++ Club/home/models.py
@@ -73,3 +73,13 @@ class Training(models.Model):
     
     class Meta:
         ordering = ['name']
+
+class Events(models.Model):
+    even_id = models.AutoField(primary_key=True)
+    event_name = models.CharField(max_length=255,null=True,blank=True)
+    start_date = models.DateTimeField(null=True,blank=True)
+    end_date = models.DateTimeField(null=True,blank=True)
+    event_type = models.CharField(max_length=10,null=True,blank=True)
+
+    def __str__(self):
+        return self.event_name
