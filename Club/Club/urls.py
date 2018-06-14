9d2d7862a39d5c1a39adf6c99844e2ba6fc7b673
Wannes Van Praet <Wannesvp@users.noreply.github.com>
2018-06-15 01:18:51 +0200

    added login and logout page

--- Club/Club/urls.py
+++ Club/Club/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from . import views
 # from django.urls import path
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^', include('home.urls')),
     # path('admin/', admin.site.urls),
 ]
