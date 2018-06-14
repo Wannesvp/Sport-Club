757408ace0e82efc80a2bbc5a6e61b937ba83322
Wannes Van Praet <Wannesvp@users.noreply.github.com>
2018-06-14 14:18:02 +0200

    changed event type field. new model type_event

--- Club/home/models.py
+++ Club/home/models.py
@@ -59,6 +59,7 @@ class Training_program(models.Model):
     class Meta:
         ordering = ['name']
 
+
 class Training(models.Model):
     name = models.CharField(max_length=60, default='AddName')
     date = models.DateField((u"Date"), default=datetime.date.today, blank=True)
@@ -74,12 +75,20 @@ class Training(models.Model):
     class Meta:
         ordering = ['name']
 
+
+class type_event(models.Model):
+    type = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.level
+
+
 class Events(models.Model):
     even_id = models.AutoField(primary_key=True)
     event_name = models.CharField(max_length=255,null=True,blank=True)
     start_date = models.DateTimeField(null=True,blank=True)
     end_date = models.DateTimeField(null=True,blank=True)
-    event_type = models.CharField(max_length=10,null=True,blank=True)
+    event_type = models.ForeignKey(type_event, on_delete=models.CASCADE,)
 
     def __str__(self):
         return self.event_name
--- Club/home/urls.py
+++ Club/home/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from home import views
 
 urlpatterns = [
-    url(r'^/$', views.home),
+    url(r'^$', views.home),
     url(r'^home/$', views.home),
     url(r'^all-trainings/$', views.display_trainings),
     url(r'^training-detail/(?P<training_id>\d+)/$', views.display_training),
