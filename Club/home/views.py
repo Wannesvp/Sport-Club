99a0eec47eddcdfea3e8eb9718904c06bf5e2d7b
Wannes Van Praet <Wannesvp@users.noreply.github.com>
2018-06-15 16:01:12 +0200

    extended user model with profile

--- Club/home/views.py
+++ Club/home/views.py
@@ -7,6 +7,9 @@ from django.contrib.auth.forms import UserCreationForm
 
 from django.contrib.auth.decorators import login_required
 
+from home.forms import UserForm, ProfileForm
+from django.db import transaction
+from django.contrib import messages
 
 '''def display(request):
     monday = []
@@ -94,3 +97,28 @@ def signup(request):
     else:
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
+
+
+@login_required
+@transaction.atomic
+def update_profile(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile was succesfullt updated!')
+            return redirect('/profile/')
+        else:
+            messages.error(request, 'Please correct the error bellow')
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = ProfileForm(instance=request.user.profile)
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form
+    }
+    return render(request, 'home/profile1.html', context)
+
+    
